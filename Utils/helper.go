2d99931ff8d9c995ddc1791e499152cc2d6e5435
crusj <296769589@qq.com>
2019-12-22 16:16:52 +0800

    update Utils

---
+++ Utils/helper.go
@@ -0,0 +1,25 @@
+package Utils
+
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+)
+
+//获取函数名称
+func GetFunctionName(i interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+}
+func ByteCountSI(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB",
+		float64(b)/float64(div), "kMGTPE"[exp])
+}
