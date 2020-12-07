6acb59ef048ec8fa3c50c8815d41966c0012bf8e
蒋龙 <jianglong@medlinker.com>
2020-12-07 22:33:38 +0800

    middleware_test

--- server_test.go
+++ server_test.go
@@ -31,30 +31,31 @@ func TestRun(t *testing.T) {
 }
 
 func handleFuncTest(ctx *fasthttp.RequestCtx) {
-	ctx.SetBody(bodyMsg)
+	ctx.Write(bodyMsg)
 }
 
 func TestGet(t *testing.T) {
-	testFunc(t, "get")
+	testFunc(t, "get", nil, nil, "")
 }
 func TestPost(t *testing.T) {
-	testFunc(t, "post")
+	testFunc(t, "post", nil, nil, "")
 }
 func TestPut(t *testing.T) {
-	testFunc(t, "put")
+	testFunc(t, "put", nil, nil, "")
 }
 func TestDelete(t *testing.T) {
-	testFunc(t, "delete")
+	testFunc(t, "delete", nil, nil, "")
+
 }
 
-func testFunc(t *testing.T, method string) {
+func testFunc(t *testing.T, method string, preMiddleWare []Router.Middleware, afterMiddleWare []Router.Middleware, expect string) {
 	Router.DefaultRouter.Register([]*Router.Route{
 		&Router.Route{
 			Method:       strings.ToUpper(method),
 			Path:         "/test_run",
 			HandleFunc:   handleFuncTest,
-			PreMiddles:   nil,
-			AfterMiddles: nil,
+			PreMiddles:   preMiddleWare,
+			AfterMiddles: afterMiddleWare,
 		},
 	}...)
 	server := NewServer(Router.DefaultRouter, "8080")
@@ -65,5 +66,35 @@ func testFunc(t *testing.T, method string) {
 	client := http.Client{}
 	response, _ := client.Do(req)
 	body, _ := ioutil.ReadAll(response.Body)
+
+	if expect != "" {
+		bodyMsg = []byte(expect)
+	}
 	assert.Equal(t, bodyMsg, body)
+}
+
+// 测试中间件
+func TestMiddleware(t *testing.T) {
+	testFunc(t, "get",
+		[]Router.Middleware{
+			middlewareMsgOne,
+		},
+		[]Router.Middleware{
+			middlewareMsgTwo,
+		},
+		"MsgOne"+string(bodyMsg)+"MsgTwo",
+	)
+}
+
+func middlewareMsgOne(handleFunc Router.HandleFunc) Router.HandleFunc {
+	return func(ctx *fasthttp.RequestCtx) {
+		ctx.Write([]byte("MsgOne"))
+		handleFunc(ctx)
+	}
+}
+func middlewareMsgTwo(handleFunc Router.HandleFunc) Router.HandleFunc {
+	return func(ctx *fasthttp.RequestCtx) {
+		handleFunc(ctx)
+		ctx.Write([]byte("MsgTwo"))
+	}
 }
